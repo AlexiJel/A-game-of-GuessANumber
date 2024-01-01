3dfbf5240afe100c0caeae1f29cb29c6ec9b1eb6
Aleksi Jeliazkov <mr.woner@gmail.com>
2024-01-01 18:27:35 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿Random random = new Random();
+int computersRandomNumber = random.Next(1, 101);
+
+Console.Write("Guess a number (1-100): ");
+int playersNumber = int.Parse(Console.ReadLine());
+
+while (playersNumber != computersRandomNumber)
+{
+    if (playersNumber < computersRandomNumber)
+    {
+        Console.WriteLine("Too Low");
+    }
+    else if (playersNumber > computersRandomNumber)
+    {
+        Console.WriteLine("Too High");
+    }
+    playersNumber = int.Parse(Console.ReadLine());
+}
+if (playersNumber == computersRandomNumber)
+{
+    Console.WriteLine("You guessed it!");
+}
